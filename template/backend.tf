05bc750b730668bff0c9f605b44061ebf3da03f0
Shaun Clarke <shaunclarke43@gmail.com>
2022-07-25 15:49:32 -0700

    updated main.tf

---
+++ template/backend.tf
@@ -0,0 +1,11 @@
+#Using terraform cloud backend to store state and prfix to reference worksapces
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "mypracticelab"
+
+    workspaces {
+      prefix = "main-"
+    }
+  }
+}
